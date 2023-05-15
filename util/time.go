21ae7f694f38b755a31f486cca00c2aa5e4a60f8
Jonathan <jpschwartz2@uwalumni.com>
2023-05-14 17:00:56 -0700

    Add epoch time to cal time

---
+++ util/time.go
@@ -0,0 +1,18 @@
+package util
+
+import (
+	"math/big"
+	"time"
+
+	"github.com/filecoin-project/lotus/build"
+)
+
+var (
+	// TODO change to mainnet
+	GENESIS_TS = uint64(1667326410)
+)
+
+func EpochHeightToTimestamp(height *big.Int) time.Time {
+	ts := (build.BlockDelaySecs * (uint64(height.Uint64()))) + GENESIS_TS
+	return time.Unix(int64(ts), 0)
+}
