1646352be7974e0815a43a02155029b4eb397f43
lanzafame <adrianlanzafame92@gmail.com>
2023-06-12 10:30:04 +1000

    remove spinner from go-pools

--- sdk/state_queries.go
+++ sdk/state_queries.go
@@ -6,7 +6,6 @@ import (
 	"math/big"
 	"time"
 
-	"github.com/briandowns/spinner"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 )
@@ -36,9 +35,6 @@ func (q *fevmQueries) StateWaitReceipt(ctx context.Context, hash common.Hash) (*
 	}
 	defer eapi.Close()
 
-	s := spinner.New(spinner.CharSets[9], 100*time.Millisecond)
-	s.Start()
-
 	// wait for 5 confirmations before getting the receipt
 	err = q.StateWaitForConfirmations(ctx, 5)
 	if err != nil {
@@ -51,11 +47,8 @@ func (q *fevmQueries) StateWaitReceipt(ctx context.Context, hash common.Hash) (*
 	var receipt *types.Receipt
 	select {
 	case <-ctx.Done():
-		s.Stop()
 		return nil, fmt.Errorf("timed out waiting for transaction")
 	case receipt = <-ch:
-		msg := fmt.Sprintf(" Receipt received at block %v.\n", receipt.BlockNumber)
-		s.Suffix = msg
 	}
 
 	if receipt == nil {
