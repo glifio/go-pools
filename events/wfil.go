48ee315e06e2a35cb84719728fc2d7bf1815833d
ganzai <ganzai@protonmail.com>
2023-07-10 12:33:32 -0400

    added get wfil transfer events

--- events/wfil.go
+++ events/wfil.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"math/big"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/glifio/go-pools/abigen"
 	"github.com/glifio/go-pools/types"
 )
@@ -34,6 +35,45 @@ func WFilDepositEvents(ctx context.Context, sdk types.PoolsSDK, startEpoch *big.
 		iter, err := filterer.FilterDeposit(getFilterOpts(ctx, i, end, sdk.Query().ChainID()), nil)
 		if err != nil {
 			return []*abigen.WFILDeposit{}, err
+		}
+
+		for iter.Next() {
+			if _, ok := hashmap[iter.Event.Raw.TxHash.Hex()]; !ok {
+				hashmap[iter.Event.Raw.TxHash.Hex()] = true
+				events = append(events, iter.Event)
+			}
+		}
+	}
+
+	return events, nil
+}
+
+func WFilTransferEvents(ctx context.Context, sdk types.PoolsSDK, startEpoch *big.Int, endEpoch *big.Int, from []common.Address, to []common.Address) ([]*abigen.WFILTransfer, error) {
+	chunkSize := big.NewInt(50000)
+
+	ethclient, err := sdk.Extern().ConnectEthClient()
+	if err != nil {
+		return []*abigen.WFILTransfer{}, err
+	}
+
+	filterer, err := abigen.NewWFILFilterer(sdk.Query().WFIL(), ethclient)
+	if err != nil {
+		return []*abigen.WFILTransfer{}, err
+	}
+
+	var events []*abigen.WFILTransfer
+	// to do - can remove hashmap logic when https://github.com/filecoin-project/lotus/issues/10964 gets merged
+	var hashmap = make(map[string]bool)
+
+	for i := startEpoch; i.Cmp(endEpoch) == -1; i.Add(i, chunkSize) {
+		end := big.NewInt(0).Add(i, chunkSize)
+		if end.Cmp(endEpoch) == 1 {
+			end = endEpoch
+		}
+
+		iter, err := filterer.FilterTransfer(getFilterOpts(ctx, i, end, sdk.Query().ChainID()), from, to) // from, to []common.Address{sdk.Query().InfinityPool()}
+		if err != nil {
+			return []*abigen.WFILTransfer{}, err
 		}
 
 		for iter.Next() {
