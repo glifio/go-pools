c0bc2af09c216451c55ba24272d9e3aae136b5c4
Jon <Schwartz10@users.noreply.github.com>
2024-02-08 21:14:11 -0800

    Add address conversion support to go pools (#67)
    * Add address conversion support to go pools
    
    * Improve TruncateAddr, add tests

--- util/addrs.go
+++ util/addrs.go
@@ -1,16 +1,25 @@
 package util
 
 import (
+	"context"
 	"crypto/ecdsa"
 	"encoding/base64"
+	"errors"
 	"fmt"
 	"log"
 	"reflect"
+	"regexp"
+	"strings"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/filecoin-project/go-address"
 	filcrypto "github.com/filecoin-project/go-crypto"
+	actorstypes "github.com/filecoin-project/go-state-types/actors"
+	"github.com/filecoin-project/go-state-types/manifest"
+	lotusapi "github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/lotus/chain/actors"
+	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/chain/types/ethtypes"
 )
 
@@ -82,11 +91,107 @@ func IsZeroAddress(address common.Address) bool {
 }
 
 func TruncateAddr(addr string) string {
-	if len(addr) <= 10 {
+	re := regexp.MustCompile(`^[tf][0]`)
+	// return ID addresses as-is
+	if re.MatchString(addr) {
 		return addr
 	}
 
 	firstSix := addr[:6]
 	lastFour := addr[len(addr)-4:]
 	return fmt.Sprintf("%s...%s", firstSix, lastFour)
+}
+
+func AddressToNative(ctx context.Context, addr string, lapi *lotusapi.FullNodeStruct, allowEVMIDs bool) (address.Address, error) {
+	re := regexp.MustCompile(`^[tf][0-9]`)
+	if re.MatchString(addr) {
+		// user passed f0, f1, f2, f3, or f4
+		filAddr, err := address.NewFromString(addr)
+		if err != nil {
+			return address.Undef, err
+		}
+
+		if !allowEVMIDs {
+			// Note that in testing, sending to an ID actor address works ok but we still block it, as this isn't intended good behavior (passing ID addrs as representations of 0x style EVM addrs)
+			if err := CheckIDNotEVMActorType(ctx, filAddr, lapi); err != nil {
+				return address.Undef, err
+			}
+		}
+
+		return filAddr, nil
+	}
+
+	if strings.HasPrefix(addr, "0x") {
+		// user passed 0x addr or account name, convert to f4
+		ethAddr, err := ethtypes.ParseEthAddress(addr)
+		if err != nil {
+			return address.Undef, err
+		}
+		return ethAddr.ToFilecoinAddress()
+	}
+
+	return address.Undef, errors.New("invalid address")
+}
+
+func AddressToEVM(ctx context.Context, addr string, lapi *lotusapi.FullNodeStruct, allowEVMIDs bool) (common.Address, error) {
+	return ParseAddress(ctx, addr, lapi, allowEVMIDs)
+}
+
+// using f0 ID addresses to interact with EVM or EthAccount actor types is forbidden
+func CheckIDNotEVMActorType(ctx context.Context, filAddr address.Address, lapi lotusapi.FullNode) error {
+	if filAddr.Protocol() == address.ID {
+		actor, err := lapi.StateGetActor(ctx, filAddr, types.EmptyTSK)
+		if err != nil {
+			return err
+		}
+
+		actorCodeEvm, success := actors.GetActorCodeID(actorstypes.Version(actors.LatestVersion), manifest.EvmKey)
+		if !success {
+			return errors.New("actor code not found")
+		}
+		if actor.Code.Equals(actorCodeEvm) {
+			return errors.New("Cant pass an ID address of an EVM actor")
+		}
+
+		actorCodeEthAccount, success := actors.GetActorCodeID(actorstypes.Version(actors.LatestVersion), manifest.EthAccountKey)
+		if !success {
+			return errors.New("actor code not found")
+		}
+		if actor.Code.Equals(actorCodeEthAccount) {
+			return errors.New("Cant pass an ID address of an Eth Account")
+		}
+	}
+
+	return nil
+}
+
+func ParseAddress(ctx context.Context, addr string, lapi lotusapi.FullNode, allowEVMIDs bool) (common.Address, error) {
+	if strings.HasPrefix(addr, "0x") {
+		return common.HexToAddress(addr), nil
+	}
+	// user passed f1, f2, f3, or f4
+	filAddr, err := address.NewFromString(addr)
+
+	if err != nil {
+		return common.Address{}, err
+	}
+
+	if !allowEVMIDs {
+		if err := CheckIDNotEVMActorType(ctx, filAddr, lapi); err != nil {
+			return common.Address{}, err
+		}
+	}
+
+	if filAddr.Protocol() != address.ID && filAddr.Protocol() != address.Delegated {
+		filAddr, err = lapi.StateLookupID(ctx, filAddr, types.EmptyTSK)
+		if err != nil {
+			return common.Address{}, err
+		}
+	}
+
+	ethAddr, err := ethtypes.EthAddressFromFilecoinAddress(filAddr)
+	if err != nil {
+		return common.Address{}, err
+	}
+	return common.HexToAddress(ethAddr.String()), nil
 }
