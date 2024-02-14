819525fefd4005a49bb7cb51f5bde5b4dc6b3391
ganzai-san <ganzai@protonmail.com>
2024-02-14 14:00:58 -0500

    remove code that handled dublicate events

--- events/ifil.go
+++ events/ifil.go
@@ -2,18 +2,12 @@ package events
 
 import (
 	"context"
-	"log"
 	"math/big"
 
 	"github.com/glifio/go-pools/abigen"
 	"github.com/glifio/go-pools/constants"
 	"github.com/glifio/go-pools/types"
 )
-
-type TxUniqueKey struct {
-	Hash  string
-	Index uint
-}
 
 func IFilTransferEvents(ctx context.Context, sdk types.PoolsSDK, startEpoch *big.Int, endEpoch *big.Int) ([]*abigen.PoolTokenTransfer, error) {
 
@@ -28,8 +22,6 @@ func IFilTransferEvents(ctx context.Context, sdk types.PoolsSDK, startEpoch *big
 	}
 
 	var events []*abigen.PoolTokenTransfer
-	// to do - can remove hashmap logic when https://github.com/filecoin-project/lotus/issues/10964 gets merged
-	var hashmap = make(map[TxUniqueKey]bool)
 
 	for i := startEpoch; i.Cmp(endEpoch) == -1; i.Add(i, constants.CHUNKSIZE) {
 		end := big.NewInt(0).Add(i, constants.CHUNKSIZE)
@@ -42,14 +34,7 @@ func IFilTransferEvents(ctx context.Context, sdk types.PoolsSDK, startEpoch *big
 			return []*abigen.PoolTokenTransfer{}, err
 		}
 
-		for iter.Next() {
-			if _, found := hashmap[TxUniqueKey{iter.Event.Raw.TxHash.Hex(), iter.Event.Raw.Index}]; !found {
-				hashmap[TxUniqueKey{iter.Event.Raw.TxHash.Hex(), iter.Event.Raw.Index}] = true
-				events = append(events, iter.Event)
-			} else {
-				log.Println("Duplicate event found", iter.Event.Raw.TxHash.Hex(), iter.Event.Raw.Index)
-			}
-		}
+		events = append(events, iter.Event)
 	}
 
 	return events, nil
