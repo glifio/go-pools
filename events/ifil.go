4b289551b90f422eb3e0fe98f7d8b7049f7ee4ef
ganzai <79986704+ganzai-san@users.noreply.github.com>
2024-02-14 11:56:32 -0800

    added function to query ifil transfer events (#69)
    * added function to query ifil transfer events
    
    * remove code that handled dublicate events

---
+++ events/ifil.go
@@ -0,0 +1,41 @@
+package events
+
+import (
+	"context"
+	"math/big"
+
+	"github.com/glifio/go-pools/abigen"
+	"github.com/glifio/go-pools/constants"
+	"github.com/glifio/go-pools/types"
+)
+
+func IFilTransferEvents(ctx context.Context, sdk types.PoolsSDK, startEpoch *big.Int, endEpoch *big.Int) ([]*abigen.PoolTokenTransfer, error) {
+
+	ethclient, err := sdk.Extern().ConnectEthClient()
+	if err != nil {
+		return []*abigen.PoolTokenTransfer{}, err
+	}
+
+	filterer, err := abigen.NewPoolTokenFilterer(sdk.Query().IFIL(), ethclient)
+	if err != nil {
+		return []*abigen.PoolTokenTransfer{}, err
+	}
+
+	var events []*abigen.PoolTokenTransfer
+
+	for i := startEpoch; i.Cmp(endEpoch) == -1; i.Add(i, constants.CHUNKSIZE) {
+		end := big.NewInt(0).Add(i, constants.CHUNKSIZE)
+		if end.Cmp(endEpoch) == 1 {
+			end = endEpoch
+		}
+
+		iter, err := filterer.FilterTransfer(getFilterOpts(ctx, i, end, sdk.Query().ChainID()), nil, nil)
+		if err != nil {
+			return []*abigen.PoolTokenTransfer{}, err
+		}
+
+		events = append(events, iter.Event)
+	}
+
+	return events, nil
+}
