9e904fd13def1b99ad2a0ac737db512dc183dae0
Jim Pick <jim@jimpick.com>
2024-07-08 22:10:16 -0700

    Calculate cost and penalty

--- terminate/onpledge.go
+++ terminate/onpledge.go
@@ -2,7 +2,6 @@ package terminate
 
 import (
 	"context"
-	"fmt"
 	"math/big"
 
 	"github.com/filecoin-project/go-state-types/abi"
@@ -23,35 +22,16 @@ func TermPenaltyOnPledge(
 	sectorSize uint64,
 	ratioVerified float64,
 ) (cost *big.Int, penalty *big.Int, err error) {
-
-	/*
-		pos, _ := new(big.Int).SetString("8875796959452500272287209423592351924294053446108449795919", 10)
-		vel, _ := new(big.Int).SetString("-3571342841146352955296449432600993921460860512773981", 10)
-		smoothedPow := smoothing.FilterEstimate{
-			PositionEstimate: filbig.NewFromGo(pos),
-			VelocityEstimate: filbig.NewFromGo(vel),
-		}
-	*/
 	smoothedPow, err := util.TotalPowerSmoothed(ctx, api, ts)
 	if err != nil {
 		return nil, nil, err
 	}
-	// fmt.Printf("Jim smoothedPow @%v: %+v\n", ts.Height(), smoothedPow)
 
 	smoothedRew, err := util.ThisEpochRewardsSmoothed(ctx, api, ts)
 	if err != nil {
 		return nil, nil, err
 	}
 
-	/*
-		pos, _ := new(big.Int).SetString("12972374068558641479895275459629742900278373039448326331292", 10)
-		vel, _ := new(big.Int).SetString("-6196791619122846434103846045826164594101002962883399", 10)
-		smoothedRew := smoothing.FilterEstimate{
-			PositionEstimate: filbig.NewFromGo(pos),
-			VelocityEstimate: filbig.NewFromGo(vel),
-		}
-	*/
-	// height := abi.ChainEpoch(4072417)
 	height := ts.Height()
 	activation := 4071894
 	expiration := 5619449
@@ -61,17 +41,11 @@ func TermPenaltyOnPledge(
 
 	dealWeight := big.NewInt(0)
 
-	// FIXME
-	// verifiedDealWeight, _ := new(big.Int).SetString("53173584910090240", 10)
-
 	scaledSectorSize := int64(float64(sectorSize) * ratioVerified)
 	verifiedDealWeight := new(big.Int).Mul(
 		big.NewInt(scaledSectorSize),
 		big.NewInt(int64(duration)),
 	)
-	fmt.Printf("Jim verifiedDealWeight: %v\n", verifiedDealWeight)
-
-	// expectedDayReward, _ := new(big.Int).SetString("1445692053274289", 10)
 
 	pwr := miner.QAPowerForWeight(
 		abi.SectorSize(sectorSize),
@@ -79,7 +53,6 @@ func TermPenaltyOnPledge(
 		filbig.NewFromGo(dealWeight),
 		filbig.NewFromGo(verifiedDealWeight),
 	)
-	fmt.Printf("Jim pwr: %v\n", pwr)
 
 	expectedDayReward := miner.ExpectedRewardForPower(
 		util.ConvertSmoothing(smoothedRew),
@@ -87,9 +60,6 @@ func TermPenaltyOnPledge(
 		pwr,
 		builtin.EpochsInDay,
 	)
-	fmt.Printf("Jim expectedDayReward: %v\n", expectedDayReward)
-
-	// expectedStoragePledge, _ := new(big.Int).SetString("28791341617364739", 10)
 
 	expectedStoragePledge := miner.ExpectedRewardForPower(
 		util.ConvertSmoothing(smoothedRew),
@@ -97,7 +67,6 @@ func TermPenaltyOnPledge(
 		pwr,
 		miner.InitialPledgeProjectionPeriod,
 	)
-	fmt.Printf("Jim expectedStoragePledge: %v\n", expectedStoragePledge)
 
 	s := miner.SectorOnChainInfo{
 		Activation:            abi.ChainEpoch(activation),
@@ -111,7 +80,6 @@ func TermPenaltyOnPledge(
 	}
 	sectorPower := miner8.QAPowerForSector(abi.SectorSize(sectorSize), util.ConvertSectorType(&s))
 
-	// the termination penalty calculation
 	termFee := miner8.PledgePenaltyForTermination(
 		s.ExpectedDayReward,
 		height-s.PowerBaseEpoch,
@@ -122,5 +90,20 @@ func TermPenaltyOnPledge(
 		s.ReplacedDayReward,
 		s.PowerBaseEpoch-s.Activation,
 	)
-	return nil, termFee.Int, nil
+
+	// Pledge for full duration
+	pledge := miner.ExpectedRewardForPower(
+		util.ConvertSmoothing(smoothedRew),
+		util.ConvertSmoothing(smoothedPow),
+		pwr,
+		duration,
+	)
+
+	// Calculate number of sectors
+	sectors := new(big.Int).Quo(filToPledge, pledge.Int)
+
+	cost = new(big.Int).Mul(pledge.Int, sectors)
+	penalty = new(big.Int).Mul(termFee.Int, sectors)
+
+	return cost, penalty, nil
 }
