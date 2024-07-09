679118b09ca4b575c7395f24f52f443eb871dec8
Jim Pick <jim@jimpick.com>
2024-07-08 20:36:09 -0700

    Get ExpectedDayReward using go-state-types

--- terminate/onpledge.go
+++ terminate/onpledge.go
@@ -1,32 +1,61 @@
 package terminate
 
 import (
+	"context"
+	"fmt"
 	"math/big"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	filbig "github.com/filecoin-project/go-state-types/big"
+	"github.com/filecoin-project/go-state-types/builtin"
 	"github.com/filecoin-project/go-state-types/builtin/v13/miner"
-	"github.com/filecoin-project/specs-actors/v6/actors/util/smoothing"
+	lotusapi "github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/lotus/chain/types"
 	miner8 "github.com/filecoin-project/specs-actors/v8/actors/builtin/miner"
 	"github.com/glifio/go-pools/util"
 )
 
-func TermPenaltyOnPledge(filToPledge *big.Int, sectorSize uint64, ratioQAP float64) (cost *big.Int, penalty *big.Int, err error) {
-	pos, _ := new(big.Int).SetString("8875796959452500272287209423592351924294053446108449795919", 10)
-	vel, _ := new(big.Int).SetString("-3571342841146352955296449432600993921460860512773981", 10)
-	smoothedPow := smoothing.FilterEstimate{
-		PositionEstimate: filbig.NewFromGo(pos),
-		VelocityEstimate: filbig.NewFromGo(vel),
+func TermPenaltyOnPledge(
+	ctx context.Context,
+	api *lotusapi.FullNodeStruct,
+	ts *types.TipSet,
+	filToPledge *big.Int,
+	sectorSize uint64,
+	ratioVerified float64,
+) (cost *big.Int, penalty *big.Int, err error) {
+
+	/*
+		pos, _ := new(big.Int).SetString("8875796959452500272287209423592351924294053446108449795919", 10)
+		vel, _ := new(big.Int).SetString("-3571342841146352955296449432600993921460860512773981", 10)
+		smoothedPow := smoothing.FilterEstimate{
+			PositionEstimate: filbig.NewFromGo(pos),
+			VelocityEstimate: filbig.NewFromGo(vel),
+		}
+	*/
+	smoothedPow, err := util.TotalPowerSmoothed(ctx, api, ts)
+	if err != nil {
+		return nil, nil, err
 	}
-	pos, _ = new(big.Int).SetString("12972374068558641479895275459629742900278373039448326331292", 10)
-	vel, _ = new(big.Int).SetString("-6196791619122846434103846045826164594101002962883399", 10)
-	smoothedRew := smoothing.FilterEstimate{
-		PositionEstimate: filbig.NewFromGo(pos),
-		VelocityEstimate: filbig.NewFromGo(vel),
+	// fmt.Printf("Jim smoothedPow @%v: %+v\n", ts.Height(), smoothedPow)
+
+	smoothedRew, err := util.ThisEpochRewardsSmoothed(ctx, api, ts)
+	if err != nil {
+		return nil, nil, err
 	}
-	height := abi.ChainEpoch(4072417)
+
+	/*
+		pos, _ := new(big.Int).SetString("12972374068558641479895275459629742900278373039448326331292", 10)
+		vel, _ := new(big.Int).SetString("-6196791619122846434103846045826164594101002962883399", 10)
+		smoothedRew := smoothing.FilterEstimate{
+			PositionEstimate: filbig.NewFromGo(pos),
+			VelocityEstimate: filbig.NewFromGo(vel),
+		}
+	*/
+	// height := abi.ChainEpoch(4072417)
+	height := ts.Height()
 	activation := 4071894
 	expiration := 5619449
+	duration := abi.ChainEpoch(expiration - activation)
 	powerBaseEpoch := activation
 	replacedDayReward := big.NewInt(0)
 
@@ -34,7 +63,25 @@ func TermPenaltyOnPledge(filToPledge *big.Int, sectorSize uint64, ratioQAP float
 
 	// FIXME
 	verifiedDealWeight, _ := new(big.Int).SetString("53173584910090240", 10)
-	expectedDayReward, _ := new(big.Int).SetString("1445692053274289", 10)
+
+	// expectedDayReward, _ := new(big.Int).SetString("1445692053274289", 10)
+
+	pwr := miner.QAPowerForWeight(
+		abi.SectorSize(sectorSize),
+		duration,
+		filbig.NewFromGo(dealWeight),
+		filbig.NewFromGo(verifiedDealWeight),
+	)
+	fmt.Printf("Jim pwr: %v\n", pwr)
+
+	expectedDayReward := miner.ExpectedRewardForPower(
+		util.ConvertSmoothing(smoothedRew),
+		util.ConvertSmoothing(smoothedPow),
+		pwr,
+		builtin.EpochsInDay,
+	)
+	fmt.Printf("Jim expectedDayReward: %v\n", expectedDayReward)
+
 	expectedStoragePledge, _ := new(big.Int).SetString("28791341617364739", 10)
 
 	s := miner.SectorOnChainInfo{
@@ -42,7 +89,7 @@ func TermPenaltyOnPledge(filToPledge *big.Int, sectorSize uint64, ratioQAP float
 		Expiration:            abi.ChainEpoch(expiration),
 		DealWeight:            abi.DealWeight(filbig.NewFromGo(dealWeight)),
 		VerifiedDealWeight:    abi.DealWeight(filbig.NewFromGo(verifiedDealWeight)),
-		ExpectedDayReward:     filbig.NewFromGo(expectedDayReward),
+		ExpectedDayReward:     expectedDayReward,
 		ExpectedStoragePledge: filbig.NewFromGo(expectedStoragePledge),
 		PowerBaseEpoch:        abi.ChainEpoch(powerBaseEpoch),
 		ReplacedDayReward:     filbig.NewFromGo(replacedDayReward),
--- terminate/onpledge_test.go
+++ terminate/onpledge_test.go
@@ -1,16 +1,40 @@
 package terminate
 
 import (
+	"context"
 	"fmt"
 	"math/big"
 	"testing"
+
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/glifio/go-pools/util"
 )
 
 func TestTermPenaltyOnPledge(t *testing.T) {
+	ctx := context.Background()
+
+	lapi, closer := util.SetupSuite(t)
+	defer util.TeardownSuite(closer)
+
+	// get ChainHead and lookback 5 epochs
+	/*
+		head, err := lapi.ChainHead(context.Background())
+		if err != nil {
+			t.Fatal(err)
+		}
+	*/
+
+	height := abi.ChainEpoch(4072417)
+	ts, err := lapi.ChainGetTipSetByHeight(context.Background(), height, types.EmptyTSK)
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	filToPledge := big.NewInt(2 * 1e18)
 	var sectorSize uint64 = 34359738368
-	var ratioQAP float64 = 1.0
-	cost, penalty, err := TermPenaltyOnPledge(filToPledge, sectorSize, ratioQAP)
+	var ratioVerified float64 = 1.0
+	cost, penalty, err := TermPenaltyOnPledge(ctx, lapi, ts, filToPledge, sectorSize, ratioVerified)
 	if err != nil {
 		t.Fatal(err)
 	}
