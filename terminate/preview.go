0cfdb28b1ef77dabb5d2029842b176b3f3cd22d8
Jim Pick <jim@jimpick.com>
2024-08-06 20:27:06 -0700

    Use v14 miner structs for waffle upgrade (#89)

--- terminate/preview.go
+++ terminate/preview.go
@@ -5,7 +5,7 @@ import (
 	"math"
 	"math/big"
 
-	"github.com/filecoin-project/go-state-types/builtin/v13/miner"
+	"github.com/filecoin-project/go-state-types/builtin/v14/miner"
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-address"
