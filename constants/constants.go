b77c7678a685e05b00bd9d50adfc44fc1919f13c
Jonathan <jpschwartz2@uwalumni.com>
2024-08-21 22:49:13 -0700

    Delete unused constants

--- constants/constants.go
+++ constants/constants.go
@@ -72,9 +72,5 @@ var CHUNKSIZE = big.NewInt(EpochsInDay)
 
 var FAULTY_SECTOR_TOLERANCE = big.NewFloat(0.001)
 
-// NOTE these should be generated in the future from the live values in the contracts
-var MAX_DTE = big.NewInt(2e18)
-var MAX_LTV = big.NewInt(8e17)
-var MAX_DTI = big.NewInt(25e16)
 var MAX_BORROW_DTL = big.NewInt(75e16)
 var LIQUIDATION_DTL = big.NewInt(85e16)
