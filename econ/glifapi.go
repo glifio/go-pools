05122543e2a8dee3386a3fb5d45ab78e803acc9a
Jonathan <jpschwartz2@uwalumni.com>
2024-09-01 20:50:08 -0700

    Potential fix for eventsapi once pools-events is redeployed
    Consolidate function calls to the api

--- econ/glifapi.go
+++ econ/glifapi.go
@@ -13,7 +13,7 @@ import (
 )
 
 func GetAgentFiFromAPI(agentAddr common.Address, eventsURL string) (*AgentFi, error) {
-	url := fmt.Sprintf("%s/agent/%s/collateral-value", eventsURL, agentAddr)
+	url := fmt.Sprintf("%s/agent/%s/margin", eventsURL, agentAddr)
 	// Making an HTTP GET request
 	resp, err := http.Get(url)
 	if err != nil {
@@ -36,51 +36,83 @@ func GetAgentFiFromAPI(agentAddr common.Address, eventsURL string) (*AgentFi, er
 		return nil, err
 	}
 
-	var response agentCollateralStats
+	var response AgentMarginJSON
 	if err := json.Unmarshal(body, &response); err != nil {
 		return nil, err
 	}
 
-	minersStats := make([]*BaseFi, len(response.MinersTerminationStats))
+	balance, ok := new(big.Int).SetString(response.Balance, 10)
+	if !ok {
+		return nil, fmt.Errorf("error converting balance to big.Int")
+	}
+
+	spendableBalance, ok := new(big.Int).SetString(response.SpendableBalance, 10)
+	if !ok {
+		return nil, fmt.Errorf("error converting spendable balance to big.Int")
+	}
 
 	agentAvail, ok := new(big.Int).SetString(response.AvailableBalance, 10)
 	if !ok {
-		return nil, fmt.Errorf("error converting agent available balance to big.Int")
+		return nil, fmt.Errorf("error converting available balance to big.Int")
 	}
 
-	// convert the miners stats to the econ package's type
-	for i, miner := range response.MinersTerminationStats {
-		m, err := convertToBigInt(miner)
-		if err != nil {
-			return nil, err
-		}
-		minersStats[i] = NewBaseFi(
-			m.Total,
-			m.Available,
-			m.Vesting,
-			m.Pledged,
-			// TODO: https://github.com/glifio/go-db/issues/3
-			big.NewInt(0),
-			m.TerminationPenalty,
-			// TODO: merge liveSectors stuff in pools-events
-			big.NewInt(0),
-			big.NewInt(0),
-		)
+	lockedRewards, ok := new(big.Int).SetString(response.LockedRewards, 10)
+	if !ok {
+		return nil, fmt.Errorf("error converting locked rewards to big.Int")
 	}
 
-	interest, principal, err := GetAgentDebtFromAPI(agentAddr, eventsURL)
-	if err != nil {
-		return nil, err
+	initialPledge, ok := new(big.Int).SetString(response.InitialPledge, 10)
+	if !ok {
+		return nil, fmt.Errorf("error converting initial pledge to big.Int")
 	}
 
-	afi := NewAgentFi(
-		agentAvail,
-		Liability{
-			Interest:  interest,
-			Principal: principal,
+	feeDebt, ok := new(big.Int).SetString(response.FeeDebt, 10)
+	if !ok {
+		return nil, fmt.Errorf("error converting fee debt to big.Int")
+	}
+
+	terminationFee, ok := new(big.Int).SetString(response.TerminationFee, 10)
+	if !ok {
+		return nil, fmt.Errorf("error converting termination fee to big.Int")
+	}
+
+	liveSectors, ok := new(big.Int).SetString(response.LiveSectors, 10)
+	if !ok {
+		return nil, fmt.Errorf("error converting live sectors to big.Int")
+	}
+
+	faultySectors, ok := new(big.Int).SetString(response.FaultySectors, 10)
+	if !ok {
+		return nil, fmt.Errorf("error converting faulty sectors to big.Int")
+	}
+
+	principal, ok := new(big.Int).SetString(response.Principal, 10)
+	if !ok {
+		return nil, fmt.Errorf("error converting principal to big.Int")
+	}
+
+	interest, ok := new(big.Int).SetString(response.Interest, 10)
+	if !ok {
+		return nil, fmt.Errorf("error converting interest to big.Int")
+	}
+
+	afi := &AgentFi{
+		BaseFi: BaseFi{
+			Balance:          balance,
+			AvailableBalance: agentAvail,
+			LockedRewards:    lockedRewards,
+			InitialPledge:    initialPledge,
+			FeeDebt:          feeDebt,
+			TerminationFee:   terminationFee,
+			LiveSectors:      liveSectors,
+			FaultySectors:    faultySectors,
 		},
-		minersStats,
-	)
+		Liability: Liability{
+			Principal: principal,
+			Interest:  interest,
+		},
+		SpendableBalance: spendableBalance,
+	}
 	return afi, nil
 }
 
@@ -151,44 +183,6 @@ func GetBaseFisFromAPI(agentAddr common.Address, eventsURL string) (miners []add
 	}
 
 	return miners, baseFis, nil
-}
-
-func GetAgentDebtFromAPI(agentAddr common.Address, eventsURL string) (interest *big.Int, principal *big.Int, err error) {
-	url := fmt.Sprintf("%s/agent/%s", eventsURL, agentAddr)
-	// Making an HTTP GET request
-	resp, err := http.Get(url)
-	if err != nil {
-		return big.NewInt(0), big.NewInt(0), err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		// if the server can't find the agent we're asking for, it will return a 404
-		// we treat it as 0 principal
-		if resp.StatusCode == http.StatusNotFound {
-			return big.NewInt(0), big.NewInt(0), nil
-		}
-
-		return big.NewInt(0), big.NewInt(0), fmt.Errorf("error fetching principal. Status code: %d", resp.StatusCode)
-	}
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return big.NewInt(0), big.NewInt(0), err
-	}
-
-	var response AgentInfo
-	if err := json.Unmarshal(body, &response); err != nil {
-		return big.NewInt(0), big.NewInt(0), err
-	}
-
-	principal, ok := new(big.Int).SetString(response.PrincipalBalance, 10)
-	if !ok {
-		return big.NewInt(0), big.NewInt(0), fmt.Errorf("error converting principal to big.Int")
-	}
-
-	//TODO: https://github.com/glifio/pools-events/issues/146 - interest is not returned by the API
-	return big.NewInt(0), principal, nil
 }
 
 type minerCollateralStat struct {
