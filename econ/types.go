d8b198df6bc60e2c0fcd937a60541650fd949098
Jonathan <jpschwartz2@uwalumni.com>
2024-09-01 20:43:48 -0700

    Add LiveSectors and FaultySectors to AgentMarginJSON

--- econ/types.go
+++ econ/types.go
@@ -75,4 +75,6 @@ type AgentMarginJSON struct {
 	BorrowLimit            string  `json:"borrowLimit"`
 	BorrowAndWithdrawLimit string  `json:"borrowAndWithdrawLimit"`
 	WithdrawLimit          string  `json:"withdrawLimit"`
+	LiveSectors            uint64  `json:"liveSectors"`
+	FaultySectors          uint64  `json:"faultySectors"`
 }
