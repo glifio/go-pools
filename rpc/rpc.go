b5251c817988e6940b35edda0d95d478b3c672ab
Jonathan <jpschwartz2@uwalumni.com>
2024-09-04 10:26:43 -0700

    Remove ado stale types

--- rpc/rpc.go
+++ rpc/rpc.go
@@ -2,20 +2,13 @@ package rpc
 
 import (
 	"context"
-	"math/big"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-jsonrpc"
 	"github.com/glifio/go-pools/abigen"
-	"github.com/glifio/go-pools/constants"
-	"github.com/glifio/go-pools/vc"
 )
 
 var ADOClient struct {
 	SignCredential func(ctx context.Context, jws string) (abigen.SignedCredential, error)
-	AgentData      func(ctx context.Context, agentAddr common.Address) (*vc.AgentData, error)
-	PreviewAction  func(ctx context.Context, agentAddr common.Address, target address.Address, value *big.Int, method constants.Method) (*vc.AgentData, error)
 }
 
 func NewADOClient(ctx context.Context, rpcurl string, namespace string) (jsonrpc.ClientCloser, error) {
