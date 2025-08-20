813d3bd60fa85dc202599448c29a9de7ae98eac6
Jim Pick <jim@jimpick.com>
2025-08-20 11:19:47 -0700

    Add PlusActivate() action

--- sdk/plus_actions.go
+++ sdk/plus_actions.go
@@ -2,6 +2,7 @@ package sdk
 
 import (
 	"context"
+	"math/big"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
@@ -40,6 +41,23 @@ func (a *fevmActions) PlusMintAndActivate(ctx context.Context, auth *bind.Transa
 	}
 
 	tx, err := plus.MintAndActivate(auth, auth.From, beneficiary, tier)
+
+	return util.TxPostProcess(tx, err)
+}
+
+func (a *fevmActions) PlusActivate(ctx context.Context, auth *bind.TransactOpts, beneficiary common.Address, tokenID *big.Int, tier uint8) (*types.Transaction, error) {
+	client, err := a.extern.ConnectEthClient()
+	if err != nil {
+		return nil, err
+	}
+	defer client.Close()
+
+	plus, err := abigen.NewPlusTransactor(a.queries.Plus(), client)
+	if err != nil {
+		return nil, err
+	}
+
+	tx, err := plus.Activate(auth, beneficiary, tokenID, tier)
 
 	return util.TxPostProcess(tx, err)
 }
